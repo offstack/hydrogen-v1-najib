9b34885539c2e0182a8c5f483424b4ef76874c6c
nurdin najib <najibnurdin999@gmail.com>
2022-07-19 12:46:41 +0700

    membuat link di product inside marque

--- src/components/sections/AllProduct.jsx
+++ src/components/sections/AllProduct.jsx
@@ -13,24 +13,26 @@ export function AllProduct () {
     return (
         <div className="container bg-orange-300 max-w-[1440px] mx-auto">
             <marquee>
-            <div class="flex">
+            <div className="flex">
                 {
                     products.nodes.map((row) => {
                         return <div className="flex-1 mr-8">
-                            
-                            <div className="card-image aspect-[4/5] bg-[#a667f4] p-4">
-                                <div className="shadow-sm rounded relative h-[300px] w-[300px]" >
-                                    <img className="w-[full] h-[280px]" src={row.featuredImage.url} alt="" />
-                                </div>
-                                <div>
-                                    <div className="w-full overflow-hidden whitespace-nowrap text-ellipsis font-[800] text-[#f8e5e5]">
-                                        {row.title.length > 29 ? row.title.slice(0, 30).concat(' ...') : row.title }
-                                        <div className="">IDR 129</div>
-                                    </div>
-                                    
-                                </div>
-                            </div>
+                                <Link to={`/products/${row.handle}`}>
 
+                                <div className="card-image aspect-[4/5] bg-[#a667f4] p-4">
+                                    <div className="shadow-sm rounded relative h-[300px] w-[300px]" >
+                                        <img className="w-[full] h-[280px]" src={row.featuredImage.url} alt="" />
+                                    </div>
+                                    <div>
+                                        <div className="w-full overflow-hidden whitespace-nowrap text-ellipsis font-[800] text-[#f8e5e5]">
+                                            {row.title.length > 29 ? row.title.slice(0, 30).concat(' ...') : row.title }
+                                            <div className="">IDR 129</div>
+                                        </div>
+                                        
+                                    </div>
+                                </div>
+
+                                </Link>
                         </div>
                     })
                 }
@@ -48,6 +50,7 @@ query FeaturedProduct {
         id
         title
         tags
+        handle
         availableForSale
         publishedAt
         compareAtPriceRange {
